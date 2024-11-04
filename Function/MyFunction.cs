9f439e1bf56784aa72c2f38b5f79bb53b11ef186
ejyatis <92034227+ejyatis@users.noreply.github.com>
2024-11-04 10:47:08 +0900

    기본 파일 업로드

---
+++ Function/MyFunction.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace MakeFunction
+{
+    /// <summary>
+    /// 
+    /// <variable name = 'X' id="X">X = 1</variable>
+    /// </summary>
+    public enum Symbol
+    {
+        X = 1,
+        Y = 2,
+        ADD = 3,
+        SUB = 4,
+        MUL = 5,
+        DIV = 6,
+        MOD = 7,
+        COS = 8,
+        SIN = 9,
+        TAN = 10,
+        LOG = 11,
+        EXP = 12,
+        POP = 13,
+        PCL = 14
+    }
+
+    public class Function
+    {
+        Symbol x;
+        Symbol y;
+        Symbol[] _expression;
+
+        public Function()
+        {
+            
+        }
+    }
+
+
+    public struct Coordinate
+    {
+        int x;
+        int y;
+
+        public Coordinate(int x, int y)
+        {
+            this.x = 0;
+            this.y = 0;
+        }
+    }
+
+    public partial class MyFunction
+    {
+        public MyFunction()
+        {
+            
+        }
+
+
+
+        private void makeFunction()
+        {
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# AutoTileMaker
+## Auto Tile Maker
+
+It's just a program to make new tile images.
+
+And also for practicing how to manage project and git.
+
+## =============
+
+타일 이미지 생성하기 위한 프로그램입니다.
+
+깃과 프로젝트 관리 연습도 할 겸 만들었습니다.
